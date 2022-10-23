46202a5e23b2bd4aea7524e654206b7449440686
PashaKenobi <81436645+PashaKenobi@users.noreply.github.com>
2022-10-23 15:37:40 +0300

    Sample Assets
    Added sample assets to test code

---
+++ Such Is Life/Assets/Player.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 5f;
+
+    public Rigidbody2D rigidbody2D;
+    public Animator animator;
+
+    Vector2 movement;
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody2D.MovePosition(rigidbody2D.position + movement * speed * Time.fixedDeltaTime);
+    }
+}
