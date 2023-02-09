0b95c90b5e87301370e735fc5830550bd42e21c0
PashaKenobi <81436645+PashaKenobi@users.noreply.github.com>
2023-02-09 12:42:50 +0300

    Hotfix

---
+++ Such Is Life/Assets/Spawn.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(player, transform.position, Quaternion.identity);
+    }
+}
